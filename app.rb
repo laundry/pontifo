7950bbc4b5185ae34ad4ca6cbea6ef2766683dc2
Andrew Fandrianto <fandrianto@gmail.com>
2014-07-28 22:02:31 -0700

    testing some shiny gems

--- app.rb
+++ app.rb
@@ -6,4 +6,9 @@ class App < Sinatra::Base
   get '/' do
     "Hello pontifo!"
   end
+
+  get '/score' do
+    halt 404 unless ![nil, ''].include?(params['guess'])
+    'Your guess of ' + params['guess'] + ' is terrible.'
+  end
 end
