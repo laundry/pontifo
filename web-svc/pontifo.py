b04bfe19271e63979edbd575b2678cffd3044de6
Andrew Fandrianto <fandrianto@gmail.com>
2014-08-02 16:49:41 -0700

    add score method to pontifo-svc

--- web-svc/pontifo.py
+++ web-svc/pontifo.py
@@ -1,8 +1,15 @@
 import os
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def pontifo():
-    return 'sup'
+    return 'hello from pontifo services'
+
+@app.route('/score', methods=['GET'])
+def score():
+    one = request.args.get('one', '')
+    two = request.args.get('two', '')
+    return "[%s] to [%s]" % (one, two)
+
