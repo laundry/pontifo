a17fb8eae4a25cb1c814e1c77c4bded5069eb76f
Tal Stramer <tstramer@andrew.cmu.edu>
2014-08-02 21:52:33 -0700

    Improve scraper

--- web-app/scripts/quote_scraper.rb
+++ web-app/scripts/quote_scraper.rb
@@ -2,6 +2,18 @@ require 'json'
 require 'nokogiri'
 require 'open-uri'
 
+def clean(txt)
+  txt.gsub(/"/, "").gsub(/\[.*?\]/, "")
+end
+
+def content(td)
+  if td.css('a').size > 0 && td.css('a')[0].content.length > 4
+    td.css('a')[0].content
+  else
+    td.content
+  end
+end
+
 URL = "http://en.wikipedia.org/wiki/AFI's_100_Years...100_Movie_Quotes"
 
 outfile = ARGV[0]
@@ -11,10 +23,10 @@ doc = Nokogiri::HTML(open(URL))
 quotes = doc.css('.wikitable tr').drop(1).collect { |row|
   cells = row.css('td')
   {
-    :text => cells[1].content.gsub(/"/, "").gsub(/\[.*?\]/, ""),
-    :character => cells[2].content,
-    :speaker => cells[3].content,
-    :from => cells[4].content
+    :text => clean(content(cells[1])),
+    :character => clean(content(cells[2])),
+    :speaker => clean(content(cells[3])),
+    :from => clean(content(cells[4]))
   }
 }
 
