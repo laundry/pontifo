a728eb7b60658bcbda2a325b641e376653f1807d
Alex <achansen121+subnt@gmail.com>
2014-08-10 14:04:32 -0700

    whoops

--- web-app/public/a.js
+++ web-app/public/a.js
@@ -62,7 +62,7 @@ intf.newgame=function(){
       intf.showquestion(intf.qobjs.length-1);
     }
   };
-  xhr.open("GET",intf.urprefix+( (!intf.qobjs.length||intf.urprefix="") ? "/game/new" : "/game/next_quote" ));
+  xhr.open("GET",intf.urprefix+( (!intf.qobjs.length||intf.urprefix=="") ? "/game/new" : "/game/next_quote" ));
   xhr.send();
 };
 
