b804ad4923bb6d00fc6652da91b3649c583c4660
Tal Stramer <tstramer@andrew.cmu.edu>
2014-08-09 22:16:34 -0700

    validation

--- web-app/app.rb
+++ web-app/app.rb
@@ -56,6 +56,10 @@ class App < Sinatra::Base
   get '/game/save' do
     content_type :json
 
+    if params[:name].nil? || params[:name].length == 0 || params[:score].nil?
+      return {:error => "You must provide name and score"}
+    end
+
     game = Game.new
     game.update_attributes(:name => params[:name], :score => params[:score])
     game.save!
